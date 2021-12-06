674bdb18dd748d92e8ed6b82ad051e1ca2c5188f
Ethan Bower <ethanbower@Ethans-MacBook-Pro.local>
2021-12-06 14:18:24 -0500

    Added shared headers security

--- index.php
+++ index.php
@@ -1,9 +1,15 @@
-<!DOCTYPE>
+<?php 
+    header("X-Frame-Options: SAMEORIGIN");
+    header("Strict-Transport-Security: max-age=1000; includeSubDomains; preload");
+    header("Content-Security-Policy: script-src; upgrade-insecure-requests");
+?>
+
+<!DOCTYPE html>
 <html>
-<head>
-    <title>My first web page</title>
-</head>
-<body>
-    <p><?php echo date("F jS, Y"); ?></p>
-</body>
+    <head>
+        <title>My first web page</title>
+    </head>
+    <body>
+        <p><?php echo date("F jS, Y"); ?></p>
+    </body>
 </html>
