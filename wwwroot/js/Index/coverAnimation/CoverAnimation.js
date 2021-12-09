af3daba820c83fc908ec180023c5c5a42d549284
Ethan Bower <ethanbower@Ethans-MacBook-Pro.local>
2021-12-08 21:46:13 -0500

    Left/right/zoom functionality. Raycasting.

--- wwwroot/js/Index/coverAnimation/CoverAnimation.js
+++ wwwroot/js/Index/coverAnimation/CoverAnimation.js
@@ -2,11 +2,17 @@ function CoverAnimation(THREE, GLTFLoader) {
     this.THREE = THREE;
     this.GLTFLoader = GLTFLoader;
     this.LoadManager = new THREE.LoadingManager();
-    this.Scene = new THREE.Scene();
-    this.Camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);//(FOV, Aspect Ratio, Near Clipping, Far Clipping ie -> Dont Render at X Distance) 
     this.Renderer = new THREE.WebGLRenderer({ antialias: true });
+    this.Camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);//(FOV, Aspect Ratio, Near Clipping, Far Clipping ie -> Dont Render at X Distance) 
+    this.Scene = new THREE.Scene();
     this.AmbientLight = new THREE.AmbientLight(0xebe8f0, 0.5);
     this.DirectionalLight = new THREE.DirectionalLight(0xebe8f0, 0.5);
+    this.RayCaster = new THREE.Raycaster();
+    this.MouseVector = new THREE.Vector2();
+    this.MouseDragSensitivity = {X: 0.05, Y: 0.05};
+    this.MouseZoomSensitivity = 0.1;
+    this.MouseWheelTracker = new MouseZoom(this.Renderer.domElement);
+    this.MouseMoveTracker = new ClickAndDrag(this.Renderer.domElement);
     this.IslandModel = new Island(this);
 
     this.init = function() {
@@ -14,7 +20,7 @@ function CoverAnimation(THREE, GLTFLoader) {
 
         //Set size will set size of canvas. Add third param and set to false to change resolution.
         //Add css canvas style to 100% for same render size but lower res.
-        this.Renderer.setSize(innerWidth, innerHeight);
+        this.Renderer.setSize(window.innerWidth, window.innerHeight);
         this.Renderer.setPixelRatio(1.5);
         this.Renderer.setClearColor(0xffffff, 0.8);
 
@@ -29,18 +35,161 @@ function CoverAnimation(THREE, GLTFLoader) {
         this.IslandModel.loadData().then(function() {
             document.body.appendChild(coverAnimationObj.Renderer.domElement);
 
-            window.addEventListener("click", function(){
-                coverAnimationObj.IslandModel.Object.rotation.x += 20;
-                coverAnimationObj.IslandModel.Object.rotation.y += 20;
-            });
+            coverAnimationObj.MouseWheelTracker.OnWheelEvent = function() {
+                if (coverAnimationObj.MouseWheelTracker.WheelHasZoomedIn()) {
+                    coverAnimationObj.Camera.position.z -= coverAnimationObj.MouseZoomSensitivity;
+                } else if (coverAnimationObj.MouseWheelTracker.WheelHasZoomedOut()) {
+                    coverAnimationObj.Camera.position.z += coverAnimationObj.MouseZoomSensitivity;
+                }
+            };
+
+            coverAnimationObj.MouseMoveTracker.OnClickAndDrag = function() {
+                if (coverAnimationObj.MouseMoveTracker.MouseHasMovedUp()) {
+                    coverAnimationObj.IslandModel.Object.rotation.x -= coverAnimationObj.MouseDragSensitivity.X;
+                } else if (coverAnimationObj.MouseMoveTracker.MouseHasMovedDown()) {
+                    coverAnimationObj.IslandModel.Object.rotation.x += coverAnimationObj.MouseDragSensitivity.X;
+                }
+
+                if (coverAnimationObj.MouseMoveTracker.MouseHasMovedRight()) {
+                    coverAnimationObj.IslandModel.Object.rotation.y += coverAnimationObj.MouseDragSensitivity.Y;
+                } else if(coverAnimationObj.MouseMoveTracker.MouseHasMovedLeft()) {
+                    coverAnimationObj.IslandModel.Object.rotation.y -= coverAnimationObj.MouseDragSensitivity.Y;
+                }
+            };
+
+            coverAnimationObj.Renderer.domElement.addEventListener('mousemove', function(e) {
+                var rect = this.getBoundingClientRect();
+
+                coverAnimationObj.MouseVector.x = ( (e.clientX - rect.left) / coverAnimationObj.Renderer.domElement.clientWidth ) * 2 - 1;
+                coverAnimationObj.MouseVector.y = - ( (e.clientY - rect.top) / coverAnimationObj.Renderer.domElement.clientHeight ) * 2 + 1;
+                console.log(e.clientX);
+
+                coverAnimationObj.RayCaster.setFromCamera(coverAnimationObj.MouseVector, coverAnimationObj.Camera);
+                var intersects = coverAnimationObj.RayCaster.intersectObjects(coverAnimationObj.Scene.children);
+
+                if (intersects.length > 0) {
+                    alert(intersects[0]);
+                }
+            }, false);
 
             runAnimationLoop(coverAnimationObj);
         }, function() {
             console.log("Crap");
         });
-    }
+    };
 
     this.init();
+}
+
+function ClickAndDrag(element) {
+    this.Element = element;
+    this.MouseIsDown = false;
+    this.MouseDownStartPos = null;
+    this.MouseDownEndPos = null;
+    this.Delta = {X: 0, Y: 0};
+    this.DeltaPrior = {X: 0, Y: 0};
+
+    this.Init = function() {
+        var classObj = this;
+
+        this.Element.addEventListener("mousedown", function() {
+            classObj.MouseIsDown = true;
+        });
+
+        this.Element.addEventListener("mouseup", function() {
+            classObj.MouseIsDown = false;
+            classObj.MouseDownStartPos = null;
+            classObj.MouseDownEndPos = null;
+        });
+
+        this.Element.addEventListener("mousemove", function(event) {
+            if(classObj.MouseIsDown) {
+                if(classObj.MouseDownStartPos == null) {
+                    classObj.MouseDownStartPos = {X: event.pageX, Y: event.pageY};
+                }
+
+                classObj.MouseDownEndPos = {X: event.pageX, Y: event.pageY};
+                classObj.DeltaPrior = classObj.Delta;
+                classObj.Delta = {
+                    X: classObj.MouseDownStartPos.X - classObj.MouseDownEndPos.X,
+                    Y: classObj.MouseDownStartPos.Y - classObj.MouseDownEndPos.Y
+                };
+
+                classObj.OnClickAndDrag();
+            }
+        });
+    }
+
+    this.MouseHasMovedUp = function() {
+        if(this.Delta.Y > this.DeltaPrior.Y) {
+            return true;
+        }
+        return false;
+    }
+
+    this.MouseHasMovedDown = function() {
+        if(this.Delta.Y < this.DeltaPrior.Y) {
+            return true;
+        }
+        return false;
+    }
+
+    this.MouseHasMovedRight = function() {
+        if(this.Delta.X < this.DeltaPrior.X) {
+            return true;
+        }
+        return false;
+    }
+
+    this.MouseHasMovedLeft = function() {
+        if(this.Delta.X > this.DeltaPrior.X) {
+            return true;
+        }
+        return false;
+    }
+
+    this.OnClickAndDrag = function() {
+        //Nothing unless overwritten
+    }
+
+    this.Init(); 
+}
+
+function MouseZoom(element) {
+    this.Element = element;
+    this.DeltaZ = 0;
+
+    this.Init = function() {
+        var classObj = this;
+
+        this.Element.addEventListener("wheel", function(event) {
+            event.preventDefault();
+
+            classObj.DeltaZ = event.deltaY;
+
+            classObj.OnWheelEvent();
+        });
+    }
+
+    this.WheelHasZoomedIn = function() {
+        if(this.DeltaZ > 0) {
+            return true;
+        }
+        return false;
+    }
+
+    this.WheelHasZoomedOut = function() {
+        if(this.DeltaZ < 0) {
+            return true;
+        }
+        return false;
+    }
+
+    this.OnWheelEvent = function() {
+        //Nothing unless overwritten
+    }
+
+    this.Init(); 
 }
 
 function runAnimationLoop(CoverAnimation) {
