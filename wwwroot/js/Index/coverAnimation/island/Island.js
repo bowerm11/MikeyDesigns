90167580f6b25fe1b5e0d81599ae984e8c4bdf49
Ethan Bower <ethanbower@Ethans-MacBook-Pro.local>
2021-12-15 19:37:25 -0500

    Added lighting

--- wwwroot/js/Index/coverAnimation/island/Island.js
+++ wwwroot/js/Index/coverAnimation/island/Island.js
@@ -1,21 +1,26 @@
 class Island extends ThreeObjectSet {
     constructor(coverAnimation) {
         super(coverAnimation);
-        this.AmbientLight = new coverAnimation.THREE.AmbientLight(0xebe8f0, 0.5);
-        this.DirectionalLight = new coverAnimation.THREE.DirectionalLight(0xebe8f0, 0.5);
+        this.Axis = new coverAnimation.THREE.AxesHelper(500);
+        this.DirectionalLight = new coverAnimation.THREE.DirectionalLight(0xffffff, .8);
+        this.DirectionalLight2 = new coverAnimation.THREE.DirectionalLight(0xffffff, .8);
         this.IslandScene = new coverAnimation.THREE.Scene();
         this.IslandModel = coverAnimation.Assets.IslandModel.scene;
 
+        this.IslandScene.add(this.Axis);
+
         this.IslandScene.add(this.IslandModel);
-        this.IslandScene.add(this.AmbientLight);
-        this.DirectionalLight.position.set(10, 20, 0);
+
+        this.DirectionalLight.position.set(0, 50, 0);
         this.IslandScene.add(this.DirectionalLight);
+        this.DirectionalLight2.position.set(0, -50, 0);
+        this.IslandScene.add(this.DirectionalLight2);
     }
 
     checkMouseHoveredComponents(rayCaster) {
         var intersects = rayCaster.intersectObject(this.IslandModel);
         if (intersects.length != 0) {
-            intersects[0].object.material.color.setHex( 0xa83232 );
+            intersects[0].object.material.color.setHex(0xa83232);
         }
     }
 
