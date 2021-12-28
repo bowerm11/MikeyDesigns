e2de7d09ad317fd95447db1eee547d69a65ba7f3
Ethan Bower <ethanbower@Ethans-Macbook-Pro.local>
2021-12-28 16:10:21 -0500

    Better err handling,logging, and headers

--- Controller/Shared/globalErrorHandler.php
+++ Controller/Shared/globalErrorHandler.php
@@ -2,23 +2,45 @@
     include_once(__DIR__ . "/preventDirectCall.php");
     include_once(__DIR__ . "/Logger.php");
 
-    function globalErrorHandler($errno, $errstr, $errfile, $errline)
+    ini_set('display_errors', false);
+
+    function generalErrorHandler($errno, $errstr, $errfile, $errline)
     {
         if (!(error_reporting() & $errno)) {
-            // This error code is not included in error_reporting, so let it fall
-            // through to the standard PHP error handler
-            //return false;
+            // This error code is not included in error_reporting, so let it fall through to the standard PHP error handler
+            return false;
         }
 
         // $errstr may need to be escaped
         $errstr = htmlspecialchars($errstr);
         Logger::Log(
-            "A global uncaught error has occured. Error Number: " . $errno . ", Error Str: " . $errstr . ", Error Line: " . $errline . ", Error File: ". $errfile,
-            "GlobalErrorHandler"
+            "A general uncaught error has occured. Error Number: " . $errno . ", Error Str: " . $errstr . ", Error Line: " . $errline . ", Error File: ". $errfile,
+            "generalErrorHandler"
         );     
+
         echo "An unknown error has occured. Please refresh.";
-        exit(1);
+        die(1);
     }
 
-    set_error_handler("globalErrorHandler");
+    function fatalHandler() {
+        $error = error_get_last();
+    
+        if($error !== NULL) {
+            $errno   = $error["type"];
+            $errstr = htmlspecialchars($error["message"]);
+            $errline = $error["line"];
+            $errfile = $error["file"];
+
+            Logger::Log(
+                "A fatal uncaught error has occured. Error Number: " . $errno . ", Error Str: " . $errstr . ", Error Line: " . $errline . ", Error File: ". $errfile,
+                "fatalHandler"
+            );     
+
+            echo "A fatal error has occured. Please refresh.";
+            die(1);
+        }
+    }
+
+    register_shutdown_function("fatalHandler");
+    set_error_handler("generalErrorHandler");
 ?>
