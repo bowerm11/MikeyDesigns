99af03bbf3bdea67b6a7699e893729c11040edb7
Ethan Bower <ethanbower@Ethans-Macbook-Pro.local>
2021-12-28 22:56:10 -0500

    js link fix

--- View/Home/index.php
+++ View/Home/index.php
@@ -19,19 +19,21 @@
         <link rel="stylesheet" href="<?php echo $homeConst->navbarCss?>">
     </head>
     <body>
-        <div id="loading-container">
-            <div class="loading-elements">
-                <img src="<?php echo $homeConst->loadingImg?>" alt="Dog Image">
-                <p class="text-center">Hello, I'm Holly and I'm loading your screen...</p>
+        <div class="t">
+            <div id="loading-container">
+                <div class="loading-elements">
+                    <img src="<?php echo $homeConst->loadingImg?>" alt="Dog Image">
+                    <p class="text-center">Hello, I'm Holly and I'm loading your screen...</p>
+                </div>
             </div>
-        </div>
-        <div id="home-container" class="navbar-removeable-section">
-            <div id="hand-helper" class="hand-graphic">
-                <img class="hand-img" src="<?php echo $homeConst->handImg?>" alt="">
-            </div>
-            <div id="canvas-container"></div>
-        </div>    
-        <?php include_once($homeConst->navBarPhp) ?>
+            <div id="home-container" class="navbar-removeable-section">
+                <div id="hand-helper" class="hand-graphic">
+                    <img class="hand-img" src="<?php echo $homeConst->handImg?>" alt="">
+                </div>
+                <div id="canvas-container"></div>
+            </div>    
+            <?php include_once($homeConst->navBarPhp) ?>
+        </div>  
     </body>
     <script type="text/javascript" src="<?php echo $homeConst->jQueryJs?>"></script>
     <script type="text/javascript" src="<?php echo $homeConst->coverAnimationJs?>"></script>
